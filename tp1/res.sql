60a6ba5ed58fd1876add9059740aa4e79877263a
JulienRouse <julien.rouse@gmail.com>
2015-02-09 17:13:21 +0100

    tp1 suite

--- tp1/res.sql
+++ tp1/res.sql
@@ -31,6 +31,50 @@ select e.deptno,e.job,sum(e.sal)
 	from emp e 
 	group by rollup(e.deptno,e.job);
 
---g) TODO
-select e.deptno, case grouping(e.job) when 1 then 'toutemploye' else e.job end as 'knjn',sum(e.sal) from emp e group by rollup(e.deptno,e.job);
+--g) 
+--select 
+--       case  
+--       	    when e.job is null  then 'toutemploye' 
+--       	    when e.job is not null then e.job 
+--       end as 'fkk'  
+--from emp e 
+--     group by rollup(e.deptno,e.job);
+--,sum(e.sal) e.deptno,
 
+--exo 2
+
+--1)
+select
+	t.annee,
+	c.cl_r,
+	p.category,
+	avg(v.pu*v.qte) ca_moyen
+from ventes v, clients c, produits p, temps t
+where v.tid = t.tid and v.cid = c.cl_id and v.pid = p.pid and (t.annee = 2009 or t.annee = 2010)
+group by rollup(t.annee, c.cl_r, p.category);
+
+--2)
+select
+	t.annee,
+	c.cl_r,
+	p.category,
+	avg(v.pu * v.qte) ca_moyen
+from ventes v, clients c, produits p, temps t
+where v.tid = t.tid and v.cid = c.cl_id and v.pid = p.pid and (t.annee = 2009 or t.annee = 2010)
+group by cube(t.annee, c.cl_r, p.category);
+
+--3)
+--select
+--	distinct rank() over (partition by t.annee, p.category order by v.qte) rank,
+--	t.annee,
+--	p.category,
+--	p.pname
+--from ventes v, produits p, temps t
+--where v.tid = t.tid and v.pid = p.pid;
+select
+	distinct rank() over(partition by p.category order by -v.qte) rank,
+	p.category,
+	p.pname,
+	v.qte
+from ventes v, produits p, temps t
+where v.tid = t.tid and v.pid = p.pid and p.category = 'Viandes';
