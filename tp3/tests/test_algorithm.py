2958a4c5bd0f523dbca897d936662efc41c969db
LeoPerard <leo.perard@gmail.com>
2015-02-26 00:53:00 +0100

    finish the K-Means for real data need improvements on the data

--- tp3/tests/test_algorithm.py
+++ tp3/tests/test_algorithm.py
@@ -12,13 +12,13 @@ class TestKMeans(unittest.TestCase) :
         factory = mock()
 
         algo = KMeans(values, k, (lambda x, y: x - y), factory)
+        algo.init_clusters(k)
+
         self.assertEquals(len(algo.clusters), 3)
 
     def test_test_distance(self) :
         values = [5, 8, 10]
         k = 3
-
-        factory = mock()
 
         x = 2
         center1 = 5
@@ -28,17 +28,172 @@ class TestKMeans(unittest.TestCase) :
         cluster2 = mock()
         cluster3 = mock()
 
-        when(cluster1).getCenter().thenReturn(center1)
-        when(cluster2).getCenter().thenReturn(center2)
-        when(cluster3).getCenter().thenReturn(center3)
+        when(cluster1).get_center().thenReturn(center1)
+        when(cluster2).get_center().thenReturn(center2)
+        when(cluster3).get_center().thenReturn(center3)
 
-        when(factory).createCluster(center1).thenReturn(cluster1)
-        when(factory).createCluster(center2).thenReturn(cluster2)
-        when(factory).createCluster(center3).thenReturn(cluster3)
+        factory = mock()
+        when(factory).create_cluster(center1).thenReturn(cluster1)
+        when(factory).create_cluster(center2).thenReturn(cluster2)
+        when(factory).create_cluster(center3).thenReturn(cluster3)
 
         algo = KMeans(values, k, (lambda x, y: x - y), factory)
+        algo.init_clusters(k)
         algo.choose_cluster(x)
 
-        verify(cluster1).addValue(x)
-        verify(cluster2, never).addValue(x)
-        verify(cluster3, never).addValue(x)
+        verify(cluster1).add_value(x)
+        verify(cluster1, never).remove_if_present(x)
+        verify(cluster2, never).add_value(x)
+        verify(cluster2).remove_if_present(x)
+        verify(cluster3, never).add_value(x)
+        verify(cluster3).remove_if_present(x)
+
+    def test_stop_iteration(self) :
+        values = [5, 8, 10]
+        k = 3
+
+        center1 = 5
+        center2 = 8
+        center3 = 10
+        cluster1 = mock()
+        cluster2 = mock()
+        cluster3 = mock()
+
+        when(cluster1).get_center().thenReturn(center1)
+        when(cluster2).get_center().thenReturn(center2)
+        when(cluster3).get_center().thenReturn(center3)
+        when(cluster1).has_changed().thenReturn(False)
+        when(cluster2).has_changed().thenReturn(False)
+        when(cluster3).has_changed().thenReturn(False)
+
+        factory = mock()
+        when(factory).create_cluster(center1).thenReturn(cluster1)
+        when(factory).create_cluster(center2).thenReturn(cluster2)
+        when(factory).create_cluster(center3).thenReturn(cluster3)
+
+        algo = KMeans(values, k, (lambda x, y: x - y), factory)
+        algo.distribute_values()
+
+        verify(cluster1, never).update_center()
+        verify(cluster2, never).update_center()
+        verify(cluster3, never).update_center()
+
+    def test_continue_iteration_and_update_center(self) :
+        values = [5, 8, 10]
+        k = 3
+
+        center1 = 5
+        center2 = 8
+        center3 = 10
+        cluster1 = mock()
+        cluster2 = mock()
+        cluster3 = mock()
+
+        when(cluster1).get_center().thenReturn(center1)
+        when(cluster2).get_center().thenReturn(center2)
+        when(cluster3).get_center().thenReturn(center3)
+        when(cluster1).has_changed().thenReturn(True).thenReturn(False)
+        when(cluster2).has_changed().thenReturn(False)
+        when(cluster3).has_changed().thenReturn(False)
+
+        factory = mock()
+        when(factory).create_cluster(center1).thenReturn(cluster1)
+        when(factory).create_cluster(center2).thenReturn(cluster2)
+        when(factory).create_cluster(center3).thenReturn(cluster3)
+
+        algo = KMeans(values, k, (lambda x, y: x - y), factory)
+        algo.distribute_values()
+
+        verify(cluster1).update_center()
+        verify(cluster2).update_center()
+        verify(cluster3).update_center()
+
+class TestCluster(unittest.TestCase) :
+
+    def test_ceation_add_the_center_in_values(self) :
+        center = 4
+
+        cluster = Cluster(center)
+        
+        self.assertEquals(len(cluster.values), 1)
+
+    def test_add_value(self) :
+        value = 5
+
+        cluster = Cluster(2)
+        cluster.add_value(value)
+
+        self.assertEquals(len(cluster.values), 2)
+
+    def test_add_value_duplicate_value(self) :
+        value = 5
+
+        cluster = Cluster(2)
+        cluster.add_value(value)
+        cluster.add_value(value)
+
+        self.assertEquals(len(cluster.values), 2)
+    
+    def test_remove_if_present(self) :
+        value = 5
+
+        cluster = Cluster(2)
+        cluster.add_value(value)
+        cluster.remove_if_present(value)
+
+        self.assertEquals(len(cluster.values), 1)
+
+    def test_remove_if_present_not_raise_exception(self) :
+        value = 5
+
+        cluster = Cluster(2)
+        try :
+            cluster.remove_if_present(value)
+        except ValueError :
+            self.fail("Should not raise the exception ValueError")
+
+    def test_remove_if_present_cant_remove_center(self) :
+        value = 5
+
+        cluster = Cluster(value)
+        self.assertRaises(CannotRemoveTheCenterError, cluster.remove_if_present, value)
+
+    def test_update_center(self) :
+        center = 1.
+        values = [3., 2., 4.]
+
+        cluster = Cluster(center)
+        for value in values :
+            cluster.add_value(value)
+
+        cluster.update_center()
+        
+        self.assertEquals(cluster.center, 2.5)
+
+    def test_add_value_change_the_state(self) :
+        value = 5
+
+        cluster = Cluster(4)
+        cluster.add_value(value)
+
+        self.assertTrue(cluster.has_changed())
+
+    def test_update_center_reset_state(self) :
+        center = 1
+        value = 5
+
+        cluster = Cluster(center)
+        cluster.add_value(value)
+        cluster.update_center()
+
+        self.assertFalse(cluster.has_changed())
+
+    def test_remove_change_the_state(self) :
+        value = 5
+
+        cluster = Cluster(4)
+        cluster.add_value(value)
+        cluster.update_center()
+        cluster.remove_if_present(value)
+
+        self.assertTrue(cluster.has_changed())
--- tp3/utils/algorithm.py
+++ tp3/utils/algorithm.py
@@ -21,22 +21,75 @@ class KMeans(object) :
         self.distance = distance
         self.factory = factory
         self.clusters = []
-        self.init_clusters(k)
 
     def init_clusters(self, k) :
+        print "Initilalization of", k, "clusters..."
         centers = random.sample(self.values, k)
         for center in centers :
-            print "Center choice:", center 
-            cluster = self.factory.createCluster(center)
+            cluster = self.factory.create_cluster(center)
             self.clusters.append(cluster)
 
     def choose_cluster(self, value) :
         min = sys.maxint
         choice = None
         for cluster in self.clusters :
-            center = cluster.getCenter()
+            center = cluster.get_center()
             dist = self.distance(center, value)
             if dist < min :
                 min = dist
                 choice = cluster
-        choice.addValue(value)
+
+        choice.add_value(value)
+
+        print "Removing", value, "from others clusters..."
+        for cluster in filter((lambda x: x != choice), self.clusters) :
+            cluster.remove_if_present(value)
+
+    def distribute_values(self) :
+        self.init_clusters(self.k)
+        while True :
+            for value in self.values :
+                self.choose_cluster(value)
+            states = map((lambda x: x.has_changed()), self.clusters)
+            if not any(states) :
+                break
+            for cluster in self.clusters :
+                cluster.update_center()
+        print "Finished!"
+        print ""
+
+class Cluster(object) :
+
+    def __init__(self, center) :
+        self.center = center
+        self.values = []
+        self.changed = False
+        self.add_value(center)
+
+    def add_value(self, value) :
+        if self.values.count(value) == 0 :
+            self.values.append(value)
+            self.changed = True
+    
+    def remove_if_present(self, value) :
+        if value == self.center :
+            raise CannotRemoveTheCenterError()
+
+        try :
+            self.changed = True
+            self.values.remove(value)
+        except ValueError :
+            pass
+
+    def get_center(self) :
+        return self.center
+
+    def update_center(self) :
+        self.changed = False
+        self.center = sum(self.values)/len(self.values)
+
+    def has_changed(self) :
+        return self.changed
+
+class CannotRemoveTheCenterError(Exception) :
+    pass
